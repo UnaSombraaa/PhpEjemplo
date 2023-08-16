ee3e7eb385dd397c979b3863e441b73f744d2524
UnaSombraaa <127241057+UnaSombraaa@users.noreply.github.com>
2023-08-16 17:49:15 -0300

    sesion

---
+++ Inicio de sesionado/Inicio-de-sesion.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Menu general</title>
+	<link rel="stylesheet" href="css/.css">
+</head>
+<body>
+	<div class="header">
+	<h3 class="titulo">Inicio de sesion</h3>
+	
+	<div class="menu">		
+	
+	<a href="form-Acceder.php">Acceder</a><br>
+    <a href="form-registracion.php">Registrarse</a><br>
+
+
+	<a href="listar.php">Listado de personas</a><br>
+
+	</div>
+	</div>
+	
+	
+</body>
+</html>
---
+++ Inicio de sesionado/listita.php
@@ -0,0 +1,43 @@
+<?php
+include 'menu.php';
+?>
+<html lang="es">
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Formulario de Altas</title>
+	<link rel="stylesheet" href="css/respuestas.css">
+	</head>
+	<body>
+	<h3 class="tablatitulo">LISTADO DE LOS REGISTRO DE LA TABLA</h3>
+
+	<table class="tabla">
+		<caption class="ttitulo">Lista de usuarios</caption>
+		<thead class="ttitulo">
+			<tr>
+			<th>ID</th>
+			<th>Correo</th>
+			<th>Contraseña</th>
+			
+			</tr>
+		</thead>
+	<?php 
+
+$base = "inicio";
+$Conexion =  mysqli_connect("localhost","root","",$base);
+
+$cadena= "SELECT * FROM persona ";
+
+$consulta = mysqli_query($Conexion,$cadena);
+
+while(  $registro = mysqli_fetch_row($consulta)   ){
+	echo "<tr class='trmain'>";
+	echo "<th>".$registro[0]."</th>"."<th>".$registro[1]."</th>"."<th>".$registro[2]."</th>"."<th>".$registro[3]."</th>";
+	echo "</tr>";
+}
+ ?>
+    </table>
+	</body>
+</html>
+
+
